2417a4240d943c323208626c2e86d6b2edb4eb42
Y700 <jdgs.1505@gmail.com>
2024-03-16 11:58:20 -0500

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Home =()=>{
+    return (
+        <header className='Home'>
+            Hola
+        </header>
+    )
+}
+
+export default Home;
